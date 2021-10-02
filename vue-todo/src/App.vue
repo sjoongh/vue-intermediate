b936b3c37f0e0738469304f6928834e471dbcb10
sjoongh <az45687@naver.com>
2021-10-02 19:23:13 +0900

    Initial commit

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <!-- camel케이스로 권장이 되어있지만, 개인 취향에 따라 pascal로 작성해도됨 -->
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>    
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue';
+import TodoInput from './components/TodoInput.vue';
+import TodoList from './components/TodoList.vue';
+import TodoFooter from './components/TodoFooter.vue';
+
+export default {
+  components: {
+    // 컴포넌트 태그명: 컴포넌트 내용
+    'TodoHeader': TodoHeader,
+    'TodoInput': TodoInput,
+    'TodoList': TodoList,
+    'TodoFooter': TodoFooter
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
