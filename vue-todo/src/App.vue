b3066dd757ade48a4227313dbe07dad0ea6710f5
sjoongh <az45687@naver.com>
2021-10-02 19:48:20 +0900

    10-02

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -25,6 +25,21 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style>
+body {
+  text-align: center;
+  background-color: white;
+}
 
+input {
+  border-style: groove;
+  width: 200px;
+}
+
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
 </style>
--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
-    input
+    <input type="text" v-model="newTodoItem">
+    <button v-on:click="addTodo">add</button>
   </div>
 </template>
 
 <script>
 export default {
-
+    data: function() {
+        return  {
+            newTodoItem: ""
+        }
+    },
+    methods: {
+        addTodo: function() {
+            console.log(this.newTodoItem);
+            // 저장하는 로직
+            // localstoreage에 동일한 key, value값으로 저장
+            localStorage.setItem(this.newTodoItem, this.newTodoItem);
+            this.newTodoItem = '';
+        }
+    }
 }
 </script>
 
